2ed607f6da56145e4459c3e4ca6319094b929474
Crowdbotics <team@crowdbotics.com>
2020-05-16 02:01:44 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/boredom_jar_17064/MainActivity.java
@@ -0,0 +1,15 @@
+package com.boredom_jar_17064;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "boredom_jar_17064";
+    }
+}
